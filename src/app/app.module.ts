9063b0b36ae25c1ab7ffb3a4b1410bcdb3d33b53
asyraf_chelah <asyraf_chelah@dell.com>
2019-09-15 12:58:00 +0800

    adding homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SliderComponent } from './slider/slider.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SliderComponent
+    SliderComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
