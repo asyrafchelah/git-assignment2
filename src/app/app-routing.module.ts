a8562e02396ff15c639f69c149346bce6116c8e7
asyraf_chelah <asyraf_chelah@dell.com>
2019-09-15 15:35:54 +0800

    conflict resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 import { UserPageComponent } from './user-page/user-page.component';
 
 
 const routes: Routes = [
-  { path: './NazmiFaiz', component: UserPageComponent}
+  { path: '', component: HomePageComponent},
+  { path: './NazmiFaiz', component: UserPageComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,18 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SliderComponent } from './slider/slider.component';
 import { UserPageComponent } from './user-page/user-page.component';
 import { ChartsModule } from 'ng2-charts';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     SliderComponent,
+<<<<<<< HEAD
     UserPageComponent
+=======
+    HomePageComponent
+>>>>>>> 9063b0b36ae25c1ab7ffb3a4b1410bcdb3d33b53
   ],
   imports: [
     BrowserModule,
